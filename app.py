43d31ddf20ddd87d1816adf9508f4ab92b0d763f
digspencer <diegoiglan@hotmail.com>
2023-06-05 15:17:37 -0300

    Mudando Totalmente a Lógica

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-import pyautogui
+from selenium import webdriver
 import time
 
-conectar = pyautogui.locateOnScreen('conectar.png')
-enviar = pyautogui.locateOnScreen('enviar.png')
-avancar = pyautogui.locateOnScreen('avançar.png')
 
-while True:
-    if conectar:
-        pyautogui.click('conectar.png')
-        time.sleep(1)
-        pyautogui.click('enviar.png')
-    elif avancar:
-        pyautogui.click('avançar.png')
-    else:
-        num_rolagem = 5
-        for n in range(num_rolagem):
-            pyautogui.scroll(-50)
+navegador = webdriver.Chrome()
+
+navegador.get(f'https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F&fromSignIn=true&trk=cold_join_sign_in')
+time.sleep(2)
+
+navegador.find_element('xpath', '//*[@id="username"]').send_keys("diegoiglan@hotmail.com")
+navegador.find_element('xpath', '//*[@id="password"]').send_keys("swordmaric4749")
+navegador.find_element('xpath', '//*[@id="organic-div"]/form/div[3]/button').click()
+navegador.find_element('xpath', '//*[@id="global-nav-typeahead"]/input').send_keys("Tech Recuiters Python")
+navegador.send_keys('keys.RETURN')
+time.sleep(101)
+
